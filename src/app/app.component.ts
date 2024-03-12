c74a22ce61e467473d3b3cc561ea3267d3cdfe67
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-12 15:12:39 -0700

    Add search highlighting functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule, FormControl, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
+import { NgbHighlight } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'students-details',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule, NgbHighlight],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'students-details';
+  filter: FormControl;
   students: Array<any> = [
     {
       "name": "Jimmy Johnson",
@@ -21,6 +26,22 @@ export class AppComponent {
       "chemistry": 22,
       "math": 33,
       "computer": 44,
+    },
+    {
+      "name": "Some Rando",
+      "city": "Johnsonville",
+      "studentImage": "",
+      "state": "VT",
+      "country": "Countrylandia",
+      "physics": 55,
+      "chemistry": 66,
+      "math": 77,
+      "computer": 88,
     }
-  ]
+  ];
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.filter = this.fb.control("", { nonNullable: true })
+  }
 }
