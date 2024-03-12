6ca0417a7b581080901ce22ae47237cc263ec14c
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-12 15:57:23 -0700

    Add side-bar component and import to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule, FormControl, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 import { NgbHighlight } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'students-details',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule, NgbHighlight],
+  imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule, NgbHighlight, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
