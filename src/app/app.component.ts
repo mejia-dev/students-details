16893df5d48b72b08b8b1d653e39122ff1c9bf75
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-13 10:18:19 -0700

    Move landing-related elements and data to new "landing" component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule, FormControl, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { SideBarComponent } from './side-bar/side-bar.component';
 
 import { NgbHighlight } from '@ng-bootstrap/ng-bootstrap';
@@ -9,41 +8,14 @@ import { NgbHighlight } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'students-details',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule, NgbHighlight, SideBarComponent],
+  imports: [CommonModule, RouterOutlet, NgbHighlight, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'students-details';
-  filter: FormControl;
-  students: Array<any> = [
-    {
-      "name": "Jimmy Johnson",
-      "city": "Johnsonville",
-      "studentImage": "",
-      "state": "VT",
-      "country": "Countrylandia",
-      "physics": 11,
-      "chemistry": 22,
-      "math": 33,
-      "computer": 44,
-    },
-    {
-      "name": "Some Rando",
-      "city": "Johnsonville",
-      "studentImage": "",
-      "state": "VT",
-      "country": "Countrylandia",
-      "physics": 55,
-      "chemistry": 66,
-      "math": 77,
-      "computer": 88,
-    }
-  ];
   sidebarExpanded: boolean = true;
   constructor(
-    private fb: FormBuilder
   ) {
-    this.filter = this.fb.control("", { nonNullable: true })
   }
 }
---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { FormsModule, FormControl, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'students-details-landing',
+  standalone: true,
+  imports: [ FormsModule, ReactiveFormsModule,],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.scss'
+})
+export class LandingComponent {
+  filter: FormControl;
+  students: Array<any> = [
+    {
+      "name": "Jimmy Johnson",
+      "city": "Johnsonville",
+      "studentImage": "",
+      "state": "VT",
+      "country": "Countrylandia",
+      "physics": 11,
+      "chemistry": 22,
+      "math": 33,
+      "computer": 44,
+    },
+    {
+      "name": "Some Rando",
+      "city": "Johnsonville",
+      "studentImage": "",
+      "state": "VT",
+      "country": "Countrylandia",
+      "physics": 55,
+      "chemistry": 66,
+      "math": 77,
+      "computer": 88,
+    }
+  ];
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.filter = this.fb.control("", { nonNullable: true })
+  }
+}
