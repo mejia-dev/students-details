c1ee159c18fb8517bb8f8e1ae27de3303dec4f70
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-13 09:46:59 -0700

    Install ag-grid and add records component

---
+++ src/app/student-records/student-records.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'students-details-student-records',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-records.component.html',
+  styleUrl: './student-records.component.scss'
+})
+export class StudentRecordsComponent {
+  
+}
