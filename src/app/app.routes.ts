2f67510cefee5bb9556951b9341a77052ee30211
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-13 15:32:29 -0700

    Add login component and route to it if unauthenticated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,13 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from "./landing/landing.component";
 import { StudentRecordsComponent } from "./student-records/student-records.component";
 import { authGuardGuard } from './auth/auth-guard.guard';
+import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
   {
     path: 'landing',
     component: LandingComponent,
