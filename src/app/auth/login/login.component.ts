d97a45c1898bb6bb37da9627dccebb8214bd4fe0
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-14 10:05:15 -0700

    Render basic authentication portal with conditional buttons

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'students-details-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  constructor(
+    public authService: AuthService
+  ) {
 
+  }
 }
