77675691b9827e870f3622463db258fa3e1f13fd
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-14 10:21:51 -0700

    Add login and logout functions to form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { CommonModule } from '@angular/common';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'students-details-login',
@@ -11,8 +12,29 @@ import { CommonModule } from '@angular/common';
 })
 export class LoginComponent {
   constructor(
-    public authService: AuthService
-  ) {
+    public authService: AuthService,
+    public router: Router
+  ) { }
 
+  login() {
+    this.authService.login().subscribe(
+      () => {
+        if (this.authService.isLoggedIn) {
+          const redirectUrl = '/dashboard';
+
+          const navigationExtras: NavigationExtras = {
+            queryParamsHandling: 'preserve',
+            preserveFragment: true
+          };
+
+          this.router.navigate([redirectUrl], navigationExtras);
+        }
+      }
+    );
   }
+
+  logout() {
+    this.authService.logOut();
+  }
+
 }
